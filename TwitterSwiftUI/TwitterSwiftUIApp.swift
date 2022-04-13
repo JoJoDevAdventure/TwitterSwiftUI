639b3e4f512e405e84e91f961d0aa780de588882
Youssef <youssef.b.air2@gmail.com>
2022-04-13 23:01:16 +0100

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Youssef Bhl on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
